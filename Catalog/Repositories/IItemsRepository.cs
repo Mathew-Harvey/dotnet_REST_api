9edab66cb86413590b84aa9f963a4e89ec91651b
Mat <mathewharvey@gmail.com>
2021-11-01 15:59:58 +0800

    delete route added

--- Catalog/Repositories/IItemsRepository.cs
+++ Catalog/Repositories/IItemsRepository.cs
@@ -15,5 +15,6 @@ namespace Catalog.Repositories
         IEnumerable<Item> GetItems();
         void CreateItem(Item item);
         void UpdateItem(Item item);
+        void DeleteItem(Guid id);
     }
 }
